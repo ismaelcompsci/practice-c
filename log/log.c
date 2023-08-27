17b32bd83fffdad49a6f3b2bf3480c492bbeef64
ismaelcompsci <ismaelcompsci@gmail.com>
2023-08-27 17:51:21 -0500

    Dynamic arrays

---
+++ log/log.c
@@ -0,0 +1,56 @@
+#include "log.h"
+
+void logger(int level, char *fmt, ...)
+{
+    char buf[200]; // this should really be sized appropriately
+                   // possibly in response to a call to vsnprintf()
+    va_list vl;
+    va_start(vl, fmt);
+
+    vsnprintf(buf, sizeof(buf), fmt, vl);
+
+    va_end(vl);
+
+    switch (level)
+    {
+    case INFO:
+        INFO_MODE ? info(buf) : NULL;
+        break;
+    case ERROR:
+        ERROR_MODE ? error(buf) : NULL;
+        break;
+    case DEBUG:
+        DEBUG_MODE ? debug(buf) : NULL;
+        break;
+    case SUCCESS:
+        SUCCESS_MODE ? success(buf) : NULL;
+        break;
+    default:
+        info(buf);
+        break;
+    }
+}
+
+void info(char *s)
+{
+    printf(BLU "[INFO] " RESET);
+    printf("%s", s);
+}
+
+void debug(char *s)
+{
+    printf(YEL "[DEBUG] " RESET);
+    printf("%s", s);
+}
+
+void success(char *s)
+{
+    printf(GRN "[SUCCESS] " RESET);
+    printf("%s", s);
+}
+
+void error(char *s)
+{
+    printf(RED "[SUCCESS] " RESET);
+    printf("%s", s);
+}
---
+++ log/log.h
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#define RED "\x1B[31m"
+#define GRN "\x1B[32m"
+#define YEL "\x1B[33m"
+#define BLU "\x1B[34m"
+#define MAG "\x1B[35m"
+#define CYN "\x1B[36m"
+#define WHT "\x1B[37m"
+#define RESET "\x1B[0m"
+
+#define INFO 1
+#define ERROR 2
+#define DEBUG 3
+#define SUCCESS 4
+
+extern int INFO_MODE;
+extern int ERROR_MODE;
+extern int DEBUG_MODE;
+extern int SUCCESS_MODE;
+
+void logger(int, char *, ...);
+void info(char *);
+void success(char *);
+void error(char *);
+void debug(char *);
